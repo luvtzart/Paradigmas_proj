45b81e94bd88370503abd973545a3da255944b53
Luiza Marinho De Mesquita <luizamesquita2339@gmail.com>
2026-03-27 22:00:45 +0000

    commit p salvar

--- poli.c
+++ poli.c
@@ -9,59 +9,151 @@ polinomio * poli_create(int grau){
         return NULL;
     }
 
-    valor->grau = grau;
-    valor->termos = 0;
-    valor->coeficientes = (int *)calloc(grau + 1, sizeof(int));
-    if (valor->coeficientes == NULL) {
+    valor -> grau = grau;
+    valor -> termos = 0;
+    valor -> coeficientes = (int *)calloc(grau + 1, sizeof(int));
+    if (valor -> coeficientes == NULL) {
         free(valor);
         return NULL;
     }
+    
     // TODO: Implemente aqui a solucao para operacao create
 
     return valor;
 }
 
 void poli_destroy(polinomio **p){
+    if(p && *p){
+        polinomio *valor = *p;
+
+        free(valor -> coeficientes);
+        free(valor);
+
+        *p = NULL;
+    }
     // TODO: Implemente aqui a solucao para operacao destroy
-
-
     return;
 }
 
 int poli_ins_termo(polinomio *p, int exp, int coef) {
     // TODO: Implemente aqui a solucao para operacao insere coeficiente
-
+    if(p == NULL || exp < 0 || exp > p->grau) {
+        return 0; // Expoente inválido
+    }
+    if( p -> coeficientes[exp] == 0 && coef != 0) {
+        p -> termos++; // incrementa o número de termos válidos
+    } else if (p -> coeficientes[exp] != 0 && coef == 0) {
+        p -> termos--; // dec o número de termos válidos
+        //return 0;
+    
+    }
+    p -> coeficientes[exp] = coef;
     return 0;
 }
 
 int poli_get_termo(polinomio *p, int exp, int *coef){
     // TODO: Implemente aqui a solucao para operacao get coeficiente
+    // int aux = p -> grau
+    if (coef != NULL && p != NULL && exp >= 0 && exp <= p -> grau && p -> coeficientes[exp] != 0){
+        *coef = p -> coeficientes[exp];
+        return 1;
+    }
 
     return 0;
 }
 
 int calcula_px(polinomio *p, int x){
-    // TODO: Implemente aqui a solucao para operacao calcula o valor de P(x)     
-    
-    return 0;
+    // TODO: Implemente aqui a solucao para operacao calcula o valor de P(x) 
+    int result = 0;
+    int aux = 1;
+    for(int i = 0; i <= p -> grau; i++){
+        result += p -> coeficientes[i] * aux;
+        aux = aux * x;
+    }  
+    return result;
 }
 
 polinomio * poli_soma(polinomio *p, polinomio *q){
     // TODO: Implemente aqui a solucao para operacao que soma dois polinomios e gera um terceiro
+    if(p == NULL || q == NULL) return NULL;
+    
+    int grau = (p -> grau > q -> grau) ? p -> grau : q -> grau; //retorna maior grau
+    polinomio *result = poli_create(grau); // cria um polinomio como resultado da equação
 
-    return NULL;
+    for(int i = 0; i <= p-> grau; i++){
+        result-> coeficientes[i] += p-> coeficientes[i]; 
+    }
+
+    for(int i = 0; i <= q->grau; i++){
+        result -> coeficientes[i] += q -> coeficientes[i];
+    }
+    // soma os resultados da soma dos polinomios
+    for(int i = 0; i <= grau; i++){
+        if(result -> coeficientes[i] != 0){
+            result -> termos++;
+        }
+    }
+
+    return result;
 }
 
 polinomio * poli_mult(polinomio *p, polinomio *q){
     // TODO: Implemente aqui a solucao para operacao que multiplica dois polinomios e gera um terceiro
+    if(p == NULL || q == NULL) return NULL;
+    
+    int grau = p-> grau + q-> grau; // soma os termos, visto que se trata da mult
+    polinomio *result = poli_create(grau);
 
-    return NULL;
+    // mesmo esquema que a soma, porém com o for integrado e no final multiplicando q e p
+    for(int i = 0; i <= p-> grau; i++){
+        for(int j = 0; j <= q-> grau; j++){
+        result-> coeficientes[i + j] += q-> coeficientes[j] * p-> coeficientes[i];
+        }
+    }
+
+    for(int i = 0; i <= grau; i++){
+        if(result -> coeficientes[i] != 0){
+            result -> termos++;
+        }
+    }
+
+    return result;
 }
 
 polinomio * poli_div(polinomio *p, polinomio *q){
     // TODO: Implemente aqui a solucao para operacao que divide dois polinomios e gera um terceiro
+    if(p == NULL || q == NULL) return NULL;
+    if(q->grau == 0 && q->coeficientes[0] == 0) return NULL; // para operação com 0!
+    
+    int grau = p-> grau - q-> grau; // diminui os termos, visto que se trata da div
+    
+    polinomio *result = poli_create(grau);
+    polinomio *resto = poli_create(grau); // novo polinomio p fzr o resto da divisao
 
-    return NULL;
+    // mesmo esquema que a soma, porém com o for integrado e no final multiplicando q e p
+    for(int i = 0; i <= p-> grau; i++){
+        resto->coeficientes[i] = p->coeficientes[i]
+    }
+        
+    for(int i = 0; i <= p-> grau; i++){
+        if(resto->coeficientes[i] != 0){
+            int coef = resto->coeficientes[i] / q->coeficientes[q->grau];
+            
+            for(int j = 0; j <= q-> grau; j++){
+                result-> coeficientes[i + j] += q-> coeficientes[j] / p-> coeficientes[i];
+            }
+        }
+    }
+    
+
+    for(int i = 0; i <= grau; i++){
+        if(result -> coeficientes[i] != 0){
+            result -> termos++; //conta os termos
+        }
+    }
+
+    poli_destroy(&resto);
+    return result; 
 }
 
 
